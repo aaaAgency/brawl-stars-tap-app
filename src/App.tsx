601eeed392b8aafd408c4551c8fd081d66b033b4
Antuhrium <antyriyman@gmail.com>
2024-06-09 10:51:30 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+import WebApp from "@twa-dev/sdk";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button
+          onClick={() =>
+            WebApp.showAlert(`Hello World! Current count is ${count}`)
+          }
+        >
+          Show Alert
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
